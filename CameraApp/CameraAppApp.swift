e992f20243789c446e8ad5e388d1b04d47a064c8
Luka <lukasalipur.dev@gmail.com>
2022-08-05 16:12:26 +0200

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Luka Šalipur on 5.8.22..
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
